77c1ce93be2c026ee74072bad402164efd7cc98f
cyr <cyr@MacBook-Pro-de-cyr.local>
2015-09-17 04:54:08 +0200

    Initial commit : projet microcms

---
+++ app/config/prod.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyr
+ * Date: 16/09/2015
+ * Time: 12:35
+ */
---
+++ src/Domain/Article.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cyr
+ * Date: 16/09/2015
+ * Time: 12:20
+ */
+
+namespace MicroCms\domain;
+
+
+class Article
+{
+
+}
